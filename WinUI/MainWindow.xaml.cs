f90030e0f47103f2792b2234d0deeea2f2ab4116
SeasterSwe <Jakobbulow01@gmail.com>
2025-05-19 14:26:20 +0200

    created winui project

---
+++ WinUI/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                btn.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 0, 0)); // Opaque red
+            }
+        }
+    }
+}
