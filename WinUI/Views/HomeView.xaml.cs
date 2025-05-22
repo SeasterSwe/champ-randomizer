601d9be0a158de45fca9ff19c2888414cea8f370
SeasterSwe <Jakobbulow01@gmail.com>
2025-05-22 15:12:25 +0200

    simple minimalsitsk popup

--- WinUI/Views/HomeView.xaml.cs
+++ WinUI/Views/HomeView.xaml.cs
@@ -1,7 +1,10 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
 using System;
 using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WinUI.Views;
 
@@ -9,6 +12,11 @@ public sealed partial class HomeView : Page, INotifyPropertyChanged
 {
     private const double ItemWidth = 236; // 220 + 16 margin (8 on each side)
     private int _columns = 1;
+
+    // Popup state
+    private CancellationTokenSource? _popupCts;
+    private Flyout? _popupFlyout;
+    private TextBlock? _popupTextBlock;
 
     public int Columns
     {
@@ -23,18 +31,16 @@ public sealed partial class HomeView : Page, INotifyPropertyChanged
         }
     }
 
+    public event PropertyChangedEventHandler? PropertyChanged;
+
     public HomeView()
     {
-        this.InitializeComponent();
-        this.SizeChanged += OnSizeChanged;
-        // Calculate initial columns
+        InitializeComponent();
+        SizeChanged += OnSizeChanged;
         Loaded += (s, e) => CalculateColumns();
     }
 
-    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
-    {
-        CalculateColumns();
-    }
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e) => CalculateColumns();
 
     private void CalculateColumns()
     {
@@ -45,40 +51,81 @@ public sealed partial class HomeView : Page, INotifyPropertyChanged
         }
     }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
-
     private void OnPropertyChanged(string propertyName)
         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
+    // Handles the random champion button click
     private void RandomChampionButton_Click(object sender, RoutedEventArgs e)
     {
-        // Find the ChampionList for the clicked button
         if (sender is Button button && button.DataContext is WinUI.Models.ChampionList list && list.Champions?.Count > 0)
         {
-            var random = new Random();
-            var champion = list.Champions[random.Next(list.Champions.Count)];
-            string champName = champion?.Name ?? "No champion found";
-
-            // Show a simple dialog with the random champion's name
-            var dialog = new ContentDialog
-            {
-                Title = "Random Champion",
-                Content = champName,
-                CloseButtonText = "OK",
-                XamlRoot = this.XamlRoot
-            };
-            _ = dialog.ShowAsync();
+            string champName = GetRandomChampionName(list);
+            ShowPopup(champName, button);
         }
         else
         {
-            var dialog = new ContentDialog
+            ShowPopup("No champion found", sender as FrameworkElement);
+        }
+    }
+
+    // Gets a random champion name from the list
+    private static string GetRandomChampionName(WinUI.Models.ChampionList list)
+    {
+        var random = new Random();
+        var champion = list.Champions![random.Next(list.Champions.Count)];
+        return champion?.Name ?? "No champion found";
+    }
+
+    // Shows a popup with the given message at the given anchor
+    private async void ShowPopup(string message, FrameworkElement? anchor)
+    {
+        CancelPreviousPopup();
+
+        EnsurePopupInitialized();
+
+        _popupTextBlock!.Text = message;
+        if (anchor != null)
+        {
+            _popupFlyout!.ShowAt(anchor);
+        }
+
+        try
+        {
+            await Task.Delay(5000, _popupCts!.Token);
+            if (!_popupCts.Token.IsCancellationRequested)
             {
-                Title = "Random Champion",
-                Content = "No champion found",
-                CloseButtonText = "OK",
-                XamlRoot = this.XamlRoot
+                _popupFlyout.Hide();
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            // Popup was replaced by a new one, ignore
+        }
+    }
+
+    // Cancels any previous popup timer
+    private void CancelPreviousPopup()
+    {
+        _popupCts?.Cancel();
+        _popupCts = new CancellationTokenSource();
+    }
+
+    // Ensures the popup and its content are created
+    private void EnsurePopupInitialized()
+    {
+        if (_popupFlyout == null)
+        {
+            _popupTextBlock = new TextBlock
+            {
+                FontSize = 16,
+                Padding = new Thickness(12)
             };
-            _ = dialog.ShowAsync();
+
+            _popupFlyout = new Flyout
+            {
+                Content = _popupTextBlock,
+                Placement = FlyoutPlacementMode.Bottom
+            };
         }
     }
 }
